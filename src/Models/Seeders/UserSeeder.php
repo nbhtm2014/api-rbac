6ff8cf7da3fdaa108e38aa22bf85c956994e03c9
nbhtm <70140129@qq.com>
2020-10-30 09:58:31 +0800

    seeder User

--- src/Models/Seeders/UserSeeder.php
+++ src/Models/Seeders/UserSeeder.php
@@ -1,14 +1,16 @@
 <?php
 /**
  * Creator htm
- * Created by 2020/10/30 9:30
+ * Created by 2020/10/30 9:55
  **/
+
+namespace Szkj\Rbac\Models\Seeders;
 
 use \Illuminate\Database\Seeder;
 use \Illuminate\Support\Facades\DB;
 use \Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class UserSeeder extends  Seeder
 {
     public function run()
     {
