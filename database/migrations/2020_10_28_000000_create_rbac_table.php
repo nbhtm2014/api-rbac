541198a2be73b715298e8b0df66c29e2a1a42268
nbhtm <70140129@qq.com>
2020-11-09 15:47:03 +0800

    add transformer

--- database/migrations/2020_10_28_000000_create_rbac_table.php
+++ database/migrations/2020_10_28_000000_create_rbac_table.php
@@ -42,58 +42,69 @@ class CreateRbacTable extends Migration
     public function up()
     {
         //create menus table
-        Schema::create($this->tableName('menus'), function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name', 255)->nullable();
-            $table->string('path', 255)->nullable();
-            $table->string('icon',100)->nullable();
-            $table->integer('pid')->default(0);
-            $table->index(['pid'],'pid');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable($this->tableName('menus'))){
+            Schema::create($this->tableName('menus'), function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('name', 255)->nullable();
+                $table->string('path', 255)->nullable();
+                $table->string('icon',100)->nullable();
+                $table->integer('pid')->default(0);
+                $table->index(['pid'],'pid');
+                $table->timestamps();
+            });
+        }
+
 
         //create roles table
-        Schema::create($this->tableName('roles'), function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name', 255);
-            $table->timestamps();
-        });
-
+        if (!Schema::hasTable($this->tableName('roles'))){
+            Schema::create($this->tableName('roles'), function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('name', 255);
+                $table->timestamps();
+            });
+        }
 
         //create roles_menus table
-        Schema::create($this->tableName('roles_menus'), function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('role_id');
-            $table->integer('menu_id');
-            $table->index(['role_id','menu_id'],'role_id');
+        if (!Schema::hasTable($this->tableName('roles_menus'))){
+            Schema::create($this->tableName('roles_menus'), function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->integer('role_id');
+                $table->integer('menu_id');
+                $table->index(['role_id','menu_id'],'role_id');
 
-            $table->timestamps();
-        });
+                $table->timestamps();
+            });
+        }
+
 
         //create roles_routes table
-        Schema::create($this->tableName('roles_routes'), function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('role_id');
-            $table->integer('route_id');
-            $table->index(['role_id','route_id'],'role_id');
+        if (!Schema::hasTable($this->tableName('roles_routes'))){
+            Schema::create($this->tableName('roles_routes'), function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->integer('role_id');
+                $table->integer('route_id');
+                $table->index(['role_id','route_id'],'role_id');
 
-            $table->timestamps();
-        });
+                $table->timestamps();
+            });
+        }
+
 
         //create routes table
-        Schema::create($this->tableName('routes'), function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('path',255);
-            $table->string('methods',50);
-            $table->string('name',255)->nullable();
-            $table->tinyInteger('default')->default(0);
-            $table->integer('pid')->default(0);
-            $table->index(['pid'],'pid');
-            $table->index(['path','name'],'path');
+        if (!Schema::hasTable($this->tableName('routes'))) {
+            Schema::create($this->tableName('routes'), function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('path',255);
+                $table->string('methods',50);
+                $table->string('name',255)->nullable();
+                $table->tinyInteger('default')->default(0);
+                $table->integer('pid')->default(0);
+                $table->index(['pid'],'pid');
+                $table->index(['path','name'],'path');
 
-            $table->timestamps();
-        });
-
+                $table->timestamps();
+            });
+        }
 
         //create routes_catalogs table
         Schema::create($this->tableName('routes_catalogs'), function (Blueprint $table) {
@@ -105,10 +116,18 @@ class CreateRbacTable extends Migration
 
 
         Schema::table('users',function (Blueprint $table){
-            $table->string('phone',100)->nullable();
-            $table->string('ip',255)->nullable();
-            $table->integer('role_id')->default(0);
-            $table->tinyInteger('superadmin')->default(0);
+            if (!Schema::hasColumn('users','phone')){
+                $table->string('phone',100)->nullable();
+            }
+            if (!Schema::hasColumn('users','phone')){
+                $table->string('ip',255)->nullable();
+            }
+            if (!Schema::hasColumn('users','phone')){
+                $table->integer('role_id')->default(0);
+            }
+            if (!Schema::hasColumn('users','phone')){
+                $table->tinyInteger('superadmin')->default(0);
+            }
         });
     }
 
--- src/routes.php
+++ src/routes.php
@@ -4,39 +4,38 @@
  * Created by 2020/10/30 10:16
  **/
 
-use Szkj\Rbac\Controllers\MenusController;
-use Szkj\Rbac\Controllers\RolesController;
-use Szkj\Rbac\Controllers\RoutesCatalogsController;
-use Szkj\Rbac\Controllers\RoutesController;
 
 $api = app('Dingo\Api\Routing\Router');
-$api->version(config('api.version'), ['middleware' => ['auth:api', 'szkj.rbac']], function ($api) {
+$api->version(config('api.version'), [
+    'middleware' => config('szkj.route.middleware'),
+    'namespace'  => config('szkj.route.namespace'),
+], function ($api) {
 
     $api->group(['prefix' => 'rbac'], function ($api) {
         /**
          * 菜单
          */
-        $api->resource('menus', MenusController::class);
+        $api->resource('menus', 'MenusController');
         /**
          * 角色
          */
-        $api->resource('roles', RolesController::class);
-        $api->post('distribution-menus', RolesController::class . '@distributionMenus');
-        $api->post('distribution-routes', RolesController::class . '@distributionRoutes');
-        $api->post('copy', RolesController::class . '@copy');
-        $api->post('routes', RolesController::class . '@getRoutes');
-        $api->post('menus', RolesController::class . '@getMenus');
+        $api->resource('roles', 'RolesController');
+        $api->post('distribution-menus', 'RolesControlle@distributionMenus');
+        $api->post('distribution-routes', 'RolesController@distributionRoutes');
+        $api->post('copy', 'RolesController@copy');
+        $api->post('routes', 'RolesController@getRoutes');
+        $api->post('menus', 'RolesController@getMenus');
         /**
          * 路由
          */
-        $api->resource('routes', RoutesController::class);
-        $api->post('renovateRoute', RoutesController::class . '@renovateRoute');
-        $api->post('list', RoutesController::class . '@list');
+        $api->resource('routes', 'RoutesController');
+        $api->post('renovateRoute', 'RoutesController@renovateRoute');
+        $api->post('list', 'RoutesController@list');
         /**
          * 路由组
          */
-        $api->resource('routes_catalogs', RoutesCatalogsController::class);
-        $api->post('distribution-routes', RoutesCatalogsController::class . '@distributionRoutes');
-        $api->post('remove', RoutesCatalogsController::class . '@remove');
+        $api->resource('routes_catalogs', 'RoutesCatalogsController');
+        $api->post('distribution-routes', 'RoutesCatalogsController@distributionRoutes');
+        $api->post('remove', 'RoutesCatalogsController@remove');
     });
 });
